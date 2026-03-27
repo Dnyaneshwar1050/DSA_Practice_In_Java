3883a94d93002ed6398ecc18cf82148e60893d7c
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-27 18:18:15 +0530

    question for practice in java

--- Main.java
+++ Main.java
@@ -39,3 +39,17 @@ class Input {
         System.out.println(name);
     }
 }
+
+
+// question for practice 
+
+class Question {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
