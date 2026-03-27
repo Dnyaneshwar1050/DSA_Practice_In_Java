95fbc1b10bfdf524229f9f43e93f68f096c4a831
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-27 17:46:27 +0530

    veraibles learning code

--- Main.java
+++ Main.java
@@ -14,3 +14,14 @@ class output1Class {
         System.out.print("* \n* *\n* * *\n* * * *");
     }
 }
+
+// veraibles
+
+public class Main {
+    public static void main(String args[]){
+        int a = 1;
+        int b = 2;
+        int sum = a + b ;
+        System.out.println(sum);
+    }
+}
