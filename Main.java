0f8f321daaab9ffe8797066da7f4dfa1986fdc82
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-27 18:52:10 +0530

    extra question to slove

--- Main.java
+++ Main.java
@@ -66,3 +66,23 @@ class radiusOfCircle {
         System.out.println(area);
     }
 }
+
+
+//Make a program that prints the table of a number that is input by the user. 
+
+class tables {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a*1);
+        System.out.println(a*2);
+        System.out.println(a*3);
+        System.out.println(a*4);
+        System.out.println(a*5);
+        System.out.println(a*6);
+        System.out.println(a*7);
+        System.out.println(a*8);
+        System.out.println(a*9);
+        System.out.println(a*10);
+    }
+}
