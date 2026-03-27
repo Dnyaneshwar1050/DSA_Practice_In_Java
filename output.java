ae8375ed169aa1e270a93538c7ed130f7fa17fae
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-27 15:36:47 +0530

    outputs in java

---
+++ output.java
@@ -0,0 +1,16 @@
+class outputClass {
+    public static void main(String args[]) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
+
+// or
+
+class output1Class {
+    public static void main(String args[]) {
+        System.out.print("* \n* *\n* * *\n* * * *");
+    }
+}
