894b60950d5658906f1ce40e5b2efc5027692072
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-27 18:43:27 +0530

    some extra question of dsa

--- Main.java
+++ Main.java
@@ -53,3 +53,16 @@ class Question {
         System.out.println(sum);
     }
 }
+
+//Make a program that takes the radius of a circle as input, calculates its radius and area and prints it as output to the user.
+
+
+class radiusOfCircle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        double pi = 3.14;
+        double area = pi*radius*radius;
+        System.out.println(area);
+    }
+}
