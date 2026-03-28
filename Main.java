7e02626a94d8bfeb3d6400df64cbf8b6cdf8e3da
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-28 23:49:51 +0530

    solved some extra questions for dsa practice

--- Main.java
+++ Main.java
@@ -86,3 +86,24 @@ class tables {
         System.out.println(a*10);
     }
 }
+
+
+
+//Make a program that prints the table of a number that is input by the user. 
+
+class tables {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a*1);
+        System.out.println(a*2);
+        System.out.println(a*3);
+        System.out.println(a*4);
+        System.out.println(a*5);
+        System.out.println(a*6);
+        System.out.println(a*7);
+        System.out.println(a*8);
+        System.out.println(a*9);
+        System.out.println(a*10);
+    }
+}
