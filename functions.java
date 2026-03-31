8de1274bdd4a12ea8e03c14fe1ab384190614057
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-31 20:08:39 +0530

    summ of number

--- functions.java
+++ functions.java
@@ -99,3 +99,27 @@ class average {
         System.out.println(average);
     }
 }
+
+// 4)Write a function to print the sum of all odd numbers from 1 to n.
+
+class sumOfOddNumber1{
+
+    public static int sumOfOddNumber(int n){
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+           if(i % 2 !=0 ){
+            sum = sum + i;
+           }
+        }
+        return sum;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = sumOfOddNumber(n);
+        System.out.println(sum);
+
+    }
+}
