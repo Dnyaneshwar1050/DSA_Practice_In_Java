eedf266a3e47b21dff5d6131c480113f115661cf
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-31 20:20:28 +0530

    whoseGreater

--- functions.java
+++ functions.java
@@ -123,3 +123,28 @@ class sumOfOddNumber1{
 
     }
 }
+
+//5)Write a function which takes in 2 numbers and returns the greater of those two.
+
+
+class greaterWhose {
+
+    public static void greaterWhose(int a, int b){
+
+        if(a>b){
+            System.out.println(a+" is greater");
+        }else{
+            System.out.println(b+" is greater");
+        }
+        
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        greaterWhose(a, b);
+
+    }
+}
