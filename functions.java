bce5c4e84605bae5651e212ab76747b4e89376d1
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-03-31 15:53:47 +0530

    solve average questions

--- functions.java
+++ functions.java
@@ -78,3 +78,24 @@ class findFactorial {
         System.out.println(factorial);
     }
 }
+
+// 2)Enter 3 numbers from the user & make a function to print their average
+
+
+class average {
+
+    public static double average(int a, int b, int c){
+        double average = (a+b+c)/3.0;
+        return average;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        double average = average(a, b, c);
+
+        System.out.println(average);
+    }
+}
