fdda01a50ae68da39b537c308148d3884b2fb252
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-04-02 18:28:37 +0530

    mtarix transpose question solving

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -73,7 +73,7 @@ class TwoDArray {
 
 
 
-class question123{
+class question01{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -184,3 +184,32 @@ class Arrays1 {
    }
 }
 
+// For a given matrix of N x M, print its transpose
+
+class transposeMatrix{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int matrix[][] = new int[n][m];
+        
+        for(int i= 0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("The transpose is : ");
+        //To print transpose
+        for(int i=0; i<n ;i++) {
+            for(int j=0; j<m; j++) {
+                System.out.print(matrix[j][i]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
