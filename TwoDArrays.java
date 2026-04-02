910b73cb2d840db4150fd5a81f4f3cb6de219e55
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-04-02 18:04:55 +0530

    DSA practise question

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -65,10 +65,122 @@ class TwoDArray {
             }
             System.out.println();
         }
-        
-
-    }
-    
+    }   
 }
 
+
+//Print the spiral order matrix as output for a given matrix of numbers. 
+
+
+
+class question123{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m =sc.nextInt();
+
+        int matrixs[][] = new int[n][m];
+
+        //input
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrixs[i][j] = sc.nextInt();
+            }
+        }
+        
+        System.out.println("spiral order : ");
+
+        int rowStart = 0;
+        int rowEnd = n-1;
+        int colStart = 0;
+        int colEnd = m-1;
+
+        //top 
+
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            for (int col=colStart; col<=colEnd; col++){
+                System.out.print(matrixs[rowStart][col]);
+            }
+            rowStart++;
+
+            for (int row=rowStart; row<=rowEnd; row++){
+                System.out.print(matrixs[row][colEnd]);
+            }
+            colEnd--;
+
+            for(int col=colEnd; col>=colStart; col--){
+                System.out.print(matrixs[rowEnd][col]);
+            }
+            rowEnd--;
+            
+
+            for(int row=rowEnd; row>=rowStart; row--){
+                System.out.print(matrixs[row][colStart]);
+            }
+            colStart++;
+            System.out.println();
+        }
+
+    }
+}
+
+
+
+
+
+class Arrays1 {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+
+
+      int matrix[][] = new int[n][m];
+      for(int i=0; i<n; i++) {
+           for(int j=0; j<m; j++) {
+               matrix[i][j] = sc.nextInt();
+           }
+      }
+
+
+      System.out.println("The Spiral Order Matrix is : ");
+      int rowStart = 0;
+      int rowEnd = n-1;
+      int colStart = 0;
+      int colEnd = m-1;
+
+
+      //To print spiral order matrix
+      while(rowStart <= rowEnd && colStart <= colEnd) {
+          //1
+          for(int col=colStart; col<=colEnd; col++) {
+              System.out.print(matrix[rowStart][col] + " ");
+          }
+          rowStart++;
+
+
+          //2
+          for(int row=rowStart; row<=rowEnd; row++) {
+              System.out.print(matrix[row][colEnd] +" ");
+          }
+          colEnd--;
+
+
+          //3
+          for(int col=colEnd; col>=colStart; col--) {
+              System.out.print(matrix[rowEnd][col] + " ");
+          }
+          rowEnd--;
+
+
+          //4
+          for(int row=rowEnd; row>=rowStart; row--) {
+              System.out.print(matrix[row][colStart] + " ");
+          }
+          colStart++;
+
+          System.out.println();
+      }
+   }
+}
 
