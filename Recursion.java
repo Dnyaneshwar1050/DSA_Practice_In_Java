9c49de62910f85900cbe8934a76f6c443dd70aa1
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-04-06 18:55:39 +0530

    learn how to work memory in recursion

--- Recursion.java
+++ Recursion.java
@@ -19,3 +19,22 @@ public class Recursion {
         printNumber(n);
     }
 }
+
+//print sum of first n natrual number
+
+class Recursion1{
+    public static void sumOfNNaturalNo(int i, int n, int sum){
+        if(i == n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        sumOfNNaturalNo(i+1, n, sum); 
+        // return;
+        System.out.println(i);
+    }
+    public static void main(String args[]){
+        sumOfNNaturalNo(1, 5, 0);
+    }
+}
