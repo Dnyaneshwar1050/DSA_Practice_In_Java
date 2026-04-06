7057737d4b64d7f2d8a5fd11acd701b39defdcf1
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-04-06 20:54:18 +0530

    print fibonacci series till nth term in DSA practice

--- Recursion.java
+++ Recursion.java
@@ -60,3 +60,29 @@ class Recursion2{
     }
 }
 
+
+
+//print fibonacci series till nth term
+
+class Recursion3{
+
+    public static void printFibonacci(int a, int b, int n){
+
+        if( n == 0){
+            return;
+        }
+
+        int c = a + b;
+        System.out.println(c);
+        printFibonacci(b, c, n-1);
+
+    }
+
+    public static void main(String args[]){
+        int a = 0; int b = 0; 
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7;
+        printFibonacci(0, 1, n-2);
+    }
+}
