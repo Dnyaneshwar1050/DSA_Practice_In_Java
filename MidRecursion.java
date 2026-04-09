3a5cb3b5d006269b761dc4d9979fb99241f61f49
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2026-04-09 08:44:48 +0530

    Q7. Print all the subsequences of a string.

--- MidRecursion.java
+++ MidRecursion.java
@@ -181,3 +181,24 @@ boolean present[] = new boolean[str.length()];
 System.out.println(removeDuplicates(str, 0, present));
 }
 }
+
+
+//Q7. Print all the subsequences of a string.
+
+class MidRecursion7 {
+public static void printSubseq(String str, int idx, String res) {
+if(idx == str.length()) {
+System.out.println(res);
+return;
+}
+//choose
+printSubseq(str, idx+1, res+str.charAt(idx));
+//don't choose
+printSubseq(str, idx+1, res);
+}
+public static void main(String args[]) {
+String str1 = "abc";
+String str2 = "aaa";
+printSubseq(str1, 0, "");
+}
+}
